32eec74f11c8a365fb08d3403c77a713a6f58a3e
Emilia Andreasson <emiliaandreasson96@gmail.com>
2026-09-15 17:08:24 +0200

    Unistalled bootstrap, installed tailwind

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+import type { Product } from "../types/product.types"
+
+type Props = {
+    product: Product;
+}
+
+export default function ProductCard({ product }: Props) {
+    return (
+        
+    )
+}
